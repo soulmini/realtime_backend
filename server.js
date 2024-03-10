7971fbb6925ecddc578bbcef9631b6cce1cbf7c4
ayush6999 <ayushjaiswal6999@gmail.com>
2024-03-10 20:59:42 +0530

    addTestRoute

--- server.js
+++ server.js
@@ -74,5 +74,10 @@ io.on('connection', (socket) => {
     });
 });
 
-const PORT = process.env.PORT || 5000;
+
+app.get('/', (req, res) => {
+    return res.json({message : "hello from backend"})
+})
+
+const PORT = 5000;
 server.listen(PORT, () => console.log(`Listening on port ${PORT}`));
